af294c68e11b41ab482c6d1242a812a16a45a8b9
kyeongju <ainimo2@gmail.com>
2023-01-13 15:57:12 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "wrapper" {
   
 output "target_group_arns" {
   description = "Map of outputs of a wrapper."
-  value       = [for v in module.wrapper : v.target_group_arns]
+  value       = concat([for v in module.wrapper : v.target_group_arns])
   # sensitive = false  # No sensitive module output found
 }
